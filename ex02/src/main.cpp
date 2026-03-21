f4f4289a5e00bd3ef3568a1116a4de69cdceaeb2
Louis De Guillebon <lde-guil@f4r11s15.clusters.42paris.fr>
2026-03-21 08:44:22 +0100

    updated mains

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -7,14 +7,10 @@
 int main(void)
 {
 	{
-		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		std::cout << "------------- Initializing -------------" << std::endl;
 		Bureaucrat *a = new Bureaucrat();
 		AForm *b = new PresidentialPardonForm("default");
 		std::cout << std::endl;
-
-		std::cout << "\033[34mTesting\033[0m" << std::endl;
-		std::cout << a;
-		std::cout << b;
 
 		try
 		{
@@ -22,31 +18,23 @@ int main(void)
 		}
 		catch(Bureaucrat::GradeTooLowException &e)
 		{
-			std::cerr << "\033[33m" << a->getName() << " was not able to sign " << b->getName() << ": " << e.what() << "\033[0m" << std::endl;
+			std::cerr << a->getName() << " was not able to sign " << b->getName() << ": " << e.what() << std::endl;
 		}
-
-		std::cout << b;
 		std::cout << std::endl;
 
-		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+		std::cout << "--------------- Deleting ---------------" << std::endl;
 		delete a;
 		delete b;
 		std::cout << std::endl;
 	}
-	std::cout << "--------------------------------------------------------------------------------------------------------------" << std::endl;
+
 	{
 		std::cout << std::endl;
 
-		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		std::cout << "------------- Initializing -------------" << std::endl;
 		Bureaucrat *a = new Bureaucrat("Assistant", 145);
 		Bureaucrat *b = new Bureaucrat("CEO", 1);
 		AForm *c = new PresidentialPardonForm("some dude");
-		std::cout << std::endl;
-
-		std::cout << "\033[34mTesting\033[0m" << std::endl;
-		std::cout << a;
-		std::cout << b;
-		std::cout << c;
 		std::cout << std::endl;
 
 		try
@@ -55,7 +43,7 @@ int main(void)
 		}
 		catch (AForm::FormNotSignedException &e)
 		{
-			std::cerr << "\033[33m" << a->getName() << " was not able to execute the AForm " << c->getName() << ": " << e.what() << "\033[0m" << std::endl;
+			std::cerr << a->getName() << " was not able to execute the AForm " << c->getName() << ": " << e.what() << std::endl;
 		}
 		std::cout << std::endl;
 		try
@@ -64,11 +52,8 @@ int main(void)
 		}
 		catch(Bureaucrat::GradeTooLowException &e)
 		{
-			std::cerr << "\033[33m" << a->getName() << " was not able to sign the AForm " << c->getName() << ": " << e.what() << "\033[0m" << std::endl;
+			std::cerr << a->getName() << " was not able to sign the AForm " << c->getName() << ": " << e.what() << std::endl;
 		}
-
-		std::cout << std::endl;
-		std::cout << c;
 		std::cout << std::endl;
 		try
 		{
@@ -76,82 +61,68 @@ int main(void)
 		}
 		catch(Bureaucrat::GradeTooLowException &e)
 		{
-			std::cerr << "\033[33m" << b->getName() << " was not able to sign the AForm " << c->getName() << ": " << e.what() << "\033[0m" << std::endl;
+			std::cerr << b->getName() << " was not able to sign the AForm " << c->getName() << ": " << e.what() << std::endl;
 		}
 		std::cout << std::endl;
-		std::cout << c;
-		std::cout << std::endl;
-
-		std::cout << std::endl;
-		b->signForm(*c);
-		std::cout << std::endl;
-
 		try
 		{
 			c->execute(*a);
 		}
 		catch(Bureaucrat::GradeTooLowException &e)
 		{
-			std::cerr << "\033[33m" << a->getName() << " was not able to execute the AForm " << c->getName() << ": " << e.what() << "\033[0m" << std::endl;
+			std::cerr << a->getName() << " was not able to execute the AForm " << c->getName() << ": " << e.what() << std::endl;
 		}
 		std::cout << std::endl;
-
 		try
 		{
 			c->execute(*b);
 		}
 		catch(Bureaucrat::GradeTooLowException &e)
 		{
-			std::cerr << "\033[33m" << b->getName() << " was not able to execute the AForm " << c->getName() << ": " << e.what() << "\033[0m" << std::endl;
+			std::cerr << b->getName() << " was not able to execute the AForm " << c->getName() << ": " << e.what() << std::endl;
 		}
 		std::cout << std::endl;
 
-		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+		std::cout << "--------------- Deleting ---------------" << std::endl;
 		delete a;
 		delete b;
 		delete c;
 		std::cout << std::endl;
 	}
-	std::cout << "--------------------------------------------------------------------------------------------------------------" << std::endl;
+
 	{
 		std::cout << std::endl;
 
-		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		std::cout << "------------- Initializing -------------" << std::endl;
 		Bureaucrat *a = new Bureaucrat("Emperor", 1);
 		PresidentialPardonForm *b = new PresidentialPardonForm("this other dude");
 		PresidentialPardonForm *c = new PresidentialPardonForm(*b);
 		std::cout << std::endl;
 
-		std::cout << "\033[34mTesting\033[0m" << std::endl;
-		std::cout << a;
-		std::cout << b;
-		std::cout << c;
+		std::cout << "------------- Simple Test --------------" << std::endl;
 		b->beSigned(*a);
 		a->signForm(*c);
 		b->execute(*a);
 		a->executeForm(*c);
 		std::cout << std::endl;
 
-		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+		std::cout << "--------------- Deleting ---------------" << std::endl;
 		delete a;
 		delete b;
 		delete c;
 		std::cout << std::endl;
 	}
-	std::cout << "--------------------------------------------------------------------------------------------------------------" << std::endl;
+
 	{
 		std::cout << std::endl;
 
-		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		std::cout << "------------- Initializing -------------" << std::endl;
 		Bureaucrat *a = new Bureaucrat("Emperor", 1);
 		RobotomyRequestForm *b = new RobotomyRequestForm("Bender");
 		ShrubberyCreationForm *c = new ShrubberyCreationForm("christmas");
 		std::cout << std::endl;
 
-		std::cout << "\033[34mTesting\033[0m" << std::endl;
-		std::cout << a;
-		std::cout << b;
-		std::cout << c;
+		std::cout << "------------- Simple Test --------------" << std::endl;
 		b->beSigned(*a);
 		a->signForm(*c);
 		for (int i= 0; i < 10; i++)
@@ -159,7 +130,7 @@ int main(void)
 		c->execute(*a);
 		std::cout << std::endl;
 
-		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+		std::cout << "--------------- Deleting ---------------" << std::endl;
 		delete a;
 		delete b;
 		delete c;
